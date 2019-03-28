517c030c3d22a5f5d90cb2972d77138bc9a9ab46
kevin <cubobrien@gmail.com>
2019-03-27 20:03:07 -0500

    make

---
+++ script.js
@@ -0,0 +1,10 @@
+/// This is a function.
+function doSomething() {
+	document.getElementById("field").style = "background:#0000FF";
+}
+
+// This is a function.
+function doSomethingElse() {
+	document.getElementById("field").style = "background:#FF0000";
+}
+
